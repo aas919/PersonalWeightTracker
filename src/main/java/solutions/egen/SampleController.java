3c0d63797b2fe36d213d62af95ec476c17ab9cf3
Ajinkya Shukla <ajinkyashukla29@gmail.com>
2017-08-14 22:48:10 -0400

    Initial commit

---
+++ src/main/java/solutions/egen/SampleController.java
@@ -0,0 +1,14 @@
+package solutions.egen;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+}
