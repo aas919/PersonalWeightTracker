39baee96fbbef375a9fb86db633de4b46d35ad4a
Ajinkya Shukla <ajinkyashukla29@gmail.com>
2017-08-14 23:48:13 -0400

    Setup morphia for mongodb and eureka server for microservices

--- src/main/java/solutions/egen/PersonalWeightTrackerApplication.java
+++ src/main/java/solutions/egen/PersonalWeightTrackerApplication.java
@@ -2,11 +2,14 @@ package solutions.egen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class PersonalWeightTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PersonalWeightTrackerApplication.class, args);
+        System.setProperty("spring.config.name", "registration-server");
+        SpringApplication.run(PersonalWeightTrackerApplication.class, args);
 	}
 }
